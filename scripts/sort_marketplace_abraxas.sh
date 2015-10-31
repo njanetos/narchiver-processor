4462bbdac443f23b5f998591276f79c948c55dcd
njanetos <nejanetos@gmail.com>
2015-10-31 17:53:21 -0400

    Small fix.

--- scripts/sort_marketplace_abraxas.sh
+++ scripts/sort_marketplace_abraxas.sh
@@ -2,21 +2,28 @@
 
 # Sorts the agora files into listings, vendors, and categories.
 
-cd raw_by_site/agora
+cd raw_by_site/abraxas
 
 rm -rf ../listings
 rm -rf ../vendors
 rm -rf ../categories
+rm -rf ../stats
 
 mkdir ../listings
 mkdir ../vendors
 mkdir ../categories
+mkdir ../stats
 
-../../scripts/update_progress.sh "Sorting agora into listings..."
-find -type f -name "*%2Fp%2F*" -exec mv '{}' ../listings \;;
-../../scripts/update_progress.sh "Sorting agora into vendors..."
+../../scripts/update_progress.sh "Sorting abraxas into listings..."
+find -type f -name "*%2Flisting%2F*" -exec mv '{}' ../listings \;;
+../../scripts/update_progress.sh "Sorting abraxas into vendors..."
 find -type f -name "*%2Fvendor%2F*" -exec mv '{}' ../vendors \;;
+../../scripts/update_progress.sh "Sorting abraxas into categories..."
+find -type f -name "*%2Fc%2F*" -exec mv '{}' ../categories \;;
+../../scripts/update_progress.sh "Sorting abraxas into stats..."
+find -type f -name "*%2Fstats%2F*" -exec mv '{}' ../stats \;;
 
 mv ../listings listings
 mv ../vendors vendors
 mv ../categories categories
+mv ../stats stats
