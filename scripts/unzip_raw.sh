4659654ee7f257ae5c43394eb50de2054b906c04
njanetos <nejanetos@gmail.com>
2015-11-03 00:17:53 -0500

    Less unzipping.

--- scripts/unzip_raw.sh
+++ scripts/unzip_raw.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Copied everything in raw_zipped/ to raw/, then goes through and recursively unzips all of those files into the same folder.
-# Deletes all CSS styling
-# Then, deletes all zip files in /raw.
+# Unzips anything that hasn't been unzipped into the folder raw
+# Then goes through and recursively unzips everything
 
-rm -rf raw && cp -r raw_zipped raw && cd raw
+mkdir -p raw && cd raw_zipped
+
+find -type f -exec sh -c 'temp=${0#./} && if [ ! -d "../raw/""${temp%.zip}" ]; then ../scripts/update_progress.sh "Found new zip file, ""$temp"", unzipping..." && unzip -q -d ../raw/"${temp%.zip}" "$0"; fi' {} \;;
+
+cd ../raw
 
 while [ "`find . -type f -name '*.zip' | wc -l`" -gt 0 ]; do
 	find -type f -name "*.zip" -exec sh -c '../scripts/update_progress.sh "Unzipping ""$1" && unzip -q -d "${1%.*}" "$1" && rm "$1"' _ {} \;;
