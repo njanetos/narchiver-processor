b5e89cce2f5ebaa0bd6a142d6b28eb85d93e25fa
Nick Janetos <nejanetos@gmail.com>
2015-11-06 20:51:27 -0700

    Better package loading.

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -1,5 +1,6 @@
-install.packages("sqldf")
-install.packages("data.table")
+list.of.packages <- c("sqldf", "data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 library(sqldf)
 library(data.table)
