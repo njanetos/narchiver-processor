18be9dde24bf2e6143597040c68db0ee36d625bf
njanetos <nejanetos@gmail.com>
2015-12-05 20:41:06 -0500

    Small change.

--- scripts/combine_market_agora.R
+++ scripts/combine_market_agora.R
@@ -101,19 +101,16 @@ rm(reviews)
 cat(paste('[combine_market_agora.R]: Sorted reviews, ', 100 - 100*round(length(reviews_$dat)/old_len, digits = 2), '% were found to be duplicates.\n', sep = ''))
 cat(paste('[combine_market_agora.R]: Cross-referencing reviews with the price 1 week before the review was left.\n'))
 # Find an estimate for each review of the price at the time it was left
-reviews_ = reviews_[order(reviews_$dat),]
 prices_$days = floor(prices_$dat / 86400)
 reviews_$days_ago = reviews_$dat - 7
 reviews_ = reviews_[order(reviews_$dat),]
 
 old_len = length(reviews_$dat)
 
-tmp = sqldf("SELECT r.vendor, r.listing, r.val, r.content, r.user_rating, p.days AS dat FROM reviews_ AS r JOIN prices_ AS p ON p.days < r.days_ago AND p.listing == r.listing AND p.vendor == r.vendor GROUP BY p.days, r.listing, r.vendor, r.val, r.content")
-
 tmp = as.data.table(sqldf("SELECT r.dat, r.vendor, r.listing, r.val, r.content, r.user_rating, p.rowid FROM reviews_ AS r
                            JOIN prices_ AS p
                                ON p.days < r.days_ago AND p.listing == r.listing
-                           GROUP BY r.dat, r.vendor, r.listing, r.val, r.content, r.user_rating"))
+                           GROUP BY r.id"))
 
 
 rm(tmp)
