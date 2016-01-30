316d48a9f42e505326e71b7b922b0e513215fe6a
Nick Janetos <nejanetos@gmail.com>
2016-01-30 18:33:17 -0500

    Add script to construct balanced panel dataset

---
+++ scripts/balanced_panel_agora.py
@@ -0,0 +1,266 @@
+# Imports
+import numpy
+from scipy.stats import norm
+from scipy.optimize import minimize
+from scipy.interpolate import interp2d
+import scipy.stats
+import matplotlib.pyplot as plt
+import matplotlib.lines
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from IPython.display import Markdown, display
+import time
+from numpy import around
+import pandas
+import statsmodels.api as stats
+import statsmodels.formula.api as smf
+import sqlite3
+import os
+from math import log
+from collections import Counter
+import datetime
+import matplotlib.dates as mdates
+import warnings
+import copy
+warnings.filterwarnings('ignore')
+
+pandas.options.mode.chained_assignment = None
+
+# Load the database
+read = sqlite3.connect(os.path.join(os.getcwd(), 'combined_market/agora.db'))
+read_cur = read.cursor()
+
+# Fetch all price information
+read_cur.execute(""" SELECT p.dat AS dat,
+                            l.category AS category,
+                            l.vendor AS vendor,
+                            p.listing AS listing,
+                            p.price AS price,
+                            p.rating AS rating,
+                            l.amount AS amount,
+                            l.quantity AS quantity,
+                            l.units AS units,
+                            p.reviews_per_day AS reviews_per_day,
+                            p.vendor_reviews_per_day AS vendor_reviews_per_day,
+                            p.dat AS normalized,
+                            p.dat AS log_normalized,
+                            p.rowid AS id,
+                            p.min_sales AS min_sales,
+                            p.max_sales AS max_sales
+                        FROM prices AS p
+                            LEFT JOIN listings AS l
+                                ON p.listing == l.rowid""")
+prices = read_cur.fetchall()
+
+# Fetch categories
+read_cur.execute("""SELECT *
+                    FROM categories""")
+categories = read_cur.fetchall()
+
+# Fetch reviews
+read_cur.execute("""SELECT r.dat,
+                           l.category,
+                           l.vendor,
+                           r.listing,
+                           p.price,
+                           p.rating,
+                           l.amount,
+                           l.quantity,
+                           l.units,
+                           p.reviews_per_day,
+                           p.vendor_reviews_per_day,
+                           p.dat AS normalized,
+                           p.dat AS log_normalized,
+                           r.rowid AS id,
+                           p.min_sales AS min_sales,
+                           p.max_sales AS max_sales
+                        FROM reviews AS r
+                            JOIN listings AS l
+                                ON l.rowid == r.listing
+                            JOIN prices AS p
+                                ON p.rowid == r.matched_price""")
+reviews = read_cur.fetchall()
+
+read_cur.execute("""SELECT v.rowid AS id,
+                           v.*,
+                           COUNT(v.rowid) AS num_reviews
+                    FROM vendors AS v
+                        JOIN listings AS l
+                            ON l.vendor == v.rowid
+                        JOIN reviews AS r
+                            ON r.listing == l.rowid
+                    GROUP BY v.rowid""")
+vendors = read_cur.fetchall()
+
+read_cur.execute("""SELECT l.rowid AS id,
+                           l.*,
+                           COUNT(r.listing) AS num_reviews
+                    FROM listings AS l
+                        JOIN reviews AS r
+                            ON r.listing = l.rowid
+                    GROUP BY r.listing""")
+listings = read_cur.fetchall()
+
+read_cur.execute("SELECT * FROM ships_from")
+ships_from = read_cur.fetchall()
+
+read_cur.execute("SELECT * FROM ships_to")
+ships_to = read_cur.fetchall()
+
+
+
+# Close the connection
+if read:
+    read.close()
+
+# Convert tuples to lists
+prices =  [ list(p) for p in prices ]
+reviews = [ list(r) for r in reviews ]
+
+# Drop impossible values
+prices =  [ p for p in prices if p[6] != 0 and p[7] != 0 ]
+reviews = [ r for r in reviews if r[6] != 0 and r[7] != 0 ]
+
+# Normalize prices and try to put things in the same units
+for p in prices:
+    p[11] = p[4] / (p[7]*p[6])
+
+    if "g" in p[8]:
+        p[11] = p[11]/1000
+        p[8] = "mg"
+        p[6] = p[6]*1000
+    elif "kg" in p[8]:
+        p[11] = p[11]/1000000
+        p[8] = "mg"
+        p[6] = p[6]*1000000
+    elif "ug" in p[8]:
+        p[11] = p[11]*1000
+        p[8] = "mg"
+        p[6] = p[6]/1000
+    elif "oz" in p[8]:
+        p[11] = p[11]/28349.5
+        p[8] = "mg"
+        p[6] = p[6]*28349.5
+    elif "lb" in p[8]:
+        p[11] = p[11]/453592
+        p[8] = "mg"
+        p[6] = p[6]*453592
+
+    # Compute logs
+    p[12] = log(p[11])
+
+# Normalize reviews and try to put things in the same units
+for r in reviews:
+    r[11] = r[4] / (r[7]*r[6])
+
+    if "g" in r[8]:
+        r[11] = r[11]/1000
+        r[8] = "mg"
+        r[6] = r[6]*1000
+    elif "kg" in r[8]:
+        r[11] = r[11]/1000000
+        r[8] = "mg"
+        r[6] = r[6]*1000000
+    elif "ug" in r[8]:
+        r[11] = r[11]*1000
+        r[8] = "mg"
+        r[6] = r[6]/1000
+    elif "oz" in r[8]:
+        r[11] = r[11]/28349.5
+        r[8] = "mg"
+        r[6] = r[6]*28349.5
+    elif "lb" in r[8]:
+        r[11] = r[11]/453592
+        r[8] = "mg"
+        r[6] = r[6]*453592
+
+    # Compute logs
+    r[12] = log(r[11])
+
+# Select the stuff we normalized
+prices = [ p for p in prices if 'mg' in p[8]]
+reviews = [ r for r in reviews if 'mg' in r[8]]
+
+# Read into panda data frame
+names = ['DATE', 'CATEGORY', 'VENDOR', 'LISTING', 'PRICE',
+         'RATING', 'AMOUNT', 'QUANTITY', 'UNITS', 'REVIEWS_PER_DAY',
+         'V_REVIEWS_PER_DAY', 'NORMALIZED', 'LOG_NORMALIZED', 'ID',
+         'MIN_SALES', 'MAX_SALES']
+prices = pandas.DataFrame(prices, columns = names)
+reviews = pandas.DataFrame(reviews, columns = names)
+
+# Read listings into panda data frame
+names = ['ID', 'TITLE', 'CATEGORY', 'VENDOR', 'UNITS', 'AMOUNT',
+         'QUANTITY', 'SHIPS_FROM', 'SHIPS_TO', 'NUM_REVIEWS']
+listings = pandas.DataFrame(listings, columns = names)
+
+# Read vendors in panda data frame
+names = ['ID', 'NAME', 'NUM_REVIEWS']
+vendors = pandas.DataFrame(vendors, columns = names)
+
+# Get min, max dates
+min_date = datetime.datetime.fromtimestamp(min(reviews['DATE'])*86400)
+max_date = datetime.datetime.fromtimestamp(max(reviews['DATE'])*86400)
+
+min_date_days = min(reviews['DATE'])
+max_date_days = max(reviews['DATE'])
+
+# Construct a balanced dataset
+
+balanced_categories = []
+
+for category_id in set(reviews['CATEGORY'].values):
+
+    prices_cat   = prices  [prices  ['CATEGORY'] == category_id]
+    reviews_cat  = reviews [reviews ['CATEGORY'] == category_id]
+    listings_cat = listings[listings['CATEGORY'] == category_id]
+
+    # Throw out extreme prices (higher than $1000 / gram)
+    prices_cat = prices_cat[prices_cat['NORMALIZED'] < 0.2]
+    reviews_cat = reviews_cat[reviews_cat['NORMALIZED'] < 0.2]
+
+    # Count the number of reviews for each vendor
+    for v in vendors['ID']:
+        vendors.set_value(vendors['ID'] == v, 'NUM_REVIEWS', len(reviews_cat[reviews_cat['VENDOR'] == v]))
+
+    # Count the number of reviews for each listing
+    for l in listings['ID']:
+        listings.set_value(listings['ID'] == l, 'NUM_REVIEWS', len(reviews_cat[reviews_cat['LISTING'] == l]))
+
+    # Throw out vendors who don't sell stuff
+    vendors = vendors[vendors['NUM_REVIEWS'] > 0]
+
+    # Construct a balanced panel data set for each vendor
+    bins_date = [numpy.round(d) for d in numpy.linspace(min_date_days, max_date_days, num = 35)]
+    bins_date = [b for b in zip(bins_date[0:-1], bins_date[1:])]
+
+    balanced = pandas.DataFrame(columns = ['VENDOR', 'DATE', 'V_REVIEWS_PER_DAY', 'NORMALIZED', 'RATING', 'REVIEWS', 'SALES', 'RATING_DIFF'])
+
+    for v in set(vendors['ID']):
+        prices_mask = prices_cat[prices_cat['VENDOR'] == v]
+        reviews_mask = reviews_cat[reviews_cat['VENDOR'] == v]
+        for date in bins_date:
+            prices_date = prices_mask[   (prices_mask['DATE'] >= date[0])
+                                       & (prices_mask['DATE'] <  date[1])]
+            reviews_date = reviews_mask[ (reviews_mask['DATE'] >= date[0])
+                                       & (reviews_mask['DATE'] <  date[1])]
+            # Find the average of each value over this particular bin
+            if (len(prices_date['V_REVIEWS_PER_DAY'].values) > 0):
+                v_reviews_per_day = numpy.mean(prices_date['V_REVIEWS_PER_DAY'].values)
+                price             = numpy.mean(prices_date['NORMALIZED'].values)
+                rating            = numpy.mean(prices_date['RATING'].values)
+                sales             = numpy.mean(prices_date['MIN_SALES'].values)
+                reviews           = len(reviews_date['RATING'].values)
+            else:
+                v_reviews_per_day = 0
+                # reviews = float('nan')
+                reviews = 0
+                price = float('nan')
+                rating = float('nan')
+                sales = float('nan')
+
+            rating_diff = 0
+
+            balanced.loc[len(balanced)] = [v, date[0], v_reviews_per_day, price, rating, reviews, sales, rating_diff]
+
+    balanced = balanced.sort_values(by = ['VENDOR', 'DATE'])
