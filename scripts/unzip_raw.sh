e62834adbd98dd24bd82d160b258223d3e1c36fa
njanetos <nejanetos@gmail.com>
2016-06-22 12:10:29 -0400

    Fix typo

--- scripts/unzip_raw.sh
+++ scripts/unzip_raw.sh
@@ -17,7 +17,7 @@ mkdir -p raw && cd raw_zipped
 
 ../scripts/update_progress.sh "Looking for new archives..."
 
-find -type f -exec sh -c 'temp=${0#./} && if [ ! -d "../raw/""${temp%.zip}" ]; then ../scripts/update_progress.sh "Found new zip file, ""$temp"", unzipping..." && unzip -q -d -o ../raw/"${temp%.zip}" "$0"; fi' {} \;;
+find -type f -exec sh -c 'temp=${0#./} && if [ ! -d "../raw/""${temp%.zip}" ]; then ../scripts/update_progress.sh "Found new zip file, ""$temp"", unzipping..." && unzip -q -o -d ../raw/"${temp%.zip}" "$0"; fi' {} \;;
 
 ../scripts/update_progress.sh "All archives unzipped. I will now recursively unzip all zip files in the folder raw."
 
@@ -25,5 +25,5 @@ cd ../raw
 
 while [ "`find . -type f -name '*.zip' | wc -l`" -gt 0 ]; do
         ../scripts/update_progress.sh "Found zip files, unzipping..."
-	find -type f -name "*.zip" -exec sh -c '../scripts/update_progress.sh "Unzipping ""$1" && unzip -q -d -o "${1%.*}" "$1" && rm "$1"' _ {} \;;
+	find -type f -name "*.zip" -exec sh -c '../scripts/update_progress.sh "Unzipping ""$1" && unzip -q -o -d "${1%.*}" "$1" && rm "$1"' _ {} \;;
 done
